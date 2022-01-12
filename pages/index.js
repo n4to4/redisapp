3f2a0a507e4db80ae7ea3c835d965e09cd3117ff
n4to4 <n4to4k@gmail.com>
2022-01-13 08:11:31 +0900

    search

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import CarForm from "../lib/CarForm";
+import SearchForm from "../lib/SearchForm";
 
 export default function Home() {
   return (
     <div>
       <h1>Create a Car</h1>
       <CarForm />
+
+      <SearchForm />
     </div>
   )
 }
